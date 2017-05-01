cf350acb6f91bcd46f8e944f606746ea56a94355
clojj <jwin1968@gmail.com>
2017-05-01 22:41:03 +0200

    adjust cron expressions for testingˆ

--- src/main/java/testbeans/Cdi1.java
+++ src/main/java/testbeans/Cdi1.java
@@ -7,7 +7,7 @@ import javax.inject.Named;
 @Named
 public class Cdi1 {
 
-    @SimpleTimer(value = "0/5 * * * * ?")
+    @SimpleTimer(value = "0/10 * * * * ?")
     public void cdiMethod() {
         System.out.println("cdiMethod in thread " + Thread.currentThread().getName());
         Utils.sleep(5000);
--- src/main/java/testbeans/Ejb2.java
+++ src/main/java/testbeans/Ejb2.java
@@ -10,7 +10,7 @@ public class Ejb2 {
     public Ejb2() {
     }
 
-    @SimpleTimer(value = "0/2 * * * * ?")
+    @SimpleTimer(value = "0/3 * * * * ?")
     public void ejb2Method() {
         System.out.println("ejb2Method in thread " + Thread.currentThread().getName());
         Utils.sleep(2000);
